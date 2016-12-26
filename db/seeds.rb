e3d689b191320ef94ffe211115d9f71401cac699
Adamenko <mangamen45@gmail.com>
2016-12-26 22:45:34 +0200

    Seed Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+n = 1
+53.times do
+
+  p = Product.find(n)
+  p.name = Faker::Book.genre
+  p.save
+
+  n += 1
+end
+
+# 50.times do
+#   name = Faker::Team.creature
+#   description = Faker::Lorem.paragraph
+#   price = Faker::Commerce.price
+#   Product.create(name: name, description: description, price: price, category_id: rand(1..4))
+# end
