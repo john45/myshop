048728bc3711856ab10f40b837433fbca147c62f
Adamenko <mangamen45@gmail.com>
2017-01-04 00:12:36 +0200

    refactor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-# 10.times do 
-#   name = Faker::Book.genre
-#   Category.create(name: name)
-# end
+10.times do
+  title = Faker::Name.first_name
+  Feature.create(title: title)
+end
 
-# 100.times do
-#   name = Faker::Book.genre
-#   description = Faker::Lorem.paragraph
-#   price = Faker::Commerce.price
-#   Product.create(name: name, description: description, price: price, category_id: rand(1..10))
-# end
+10.times do
+  name = Faker::Name.first_name
+  Category.create(name: name)
+end
 
-# n = 1
-# 200.times do
-#   p = Product.find(n)
-#   reviews = rand(1..20)
+50.times do
+  name = Faker::Book.genre
+  description = Faker::Lorem.paragraph
+  price = Faker::Commerce.price
+  Product.create(name: name, description: description, price: price, category_id: rand(1..10))
+end
 
-#   reviews.times do
-#     email = Faker::Internet.email
-#     author = Faker::Name.first_name + ' ' + Faker::Name.last_name
-#     product_id = p.id
-#     body = Faker::Lorem.paragraphs
-#     rating = rand(1..5)
-#     published = true
+n = 1
+50.times do
+  p = Product.find(n)
+  title = Faker::Color.color_name
+  FeatureValue.create(feature_id: rand(1..10), product_id: n, title: title)
 
-#     Review.create(author: author, product_id: product_id, body: body, rating: rating, email: email, published: published)
-#   end
-#   n += 1
-# end
+  reviews = rand(1..20)
+  reviews.times do
+    email = Faker::Internet.email
+    author = Faker::Name.first_name + ' ' + Faker::Name.last_name
+    product_id = p.id
+    body = Faker::Lorem.paragraphs
+    rating = rand(1..5)
+    published = true
+
+    Review.create(author: author, product_id: product_id, body: body, rating: rating, email: email, published: published)
+  end
+  n += 1
+end
