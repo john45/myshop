02429c4f7f2f8c4057cd480b519982851f424d31
Evgeniy <mangamen45@gmail.com>
2017-01-06 13:04:06 +0000

    temp commit from work

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,23 @@ class OrdersController < ApplicationController
     @order = Order.new
     @category = Category.all
   end
+    
+  def create
+    @order = Order.new(order_params)
+    @order.add_order_product_from_cart(current_cart)
+    
+    if @order.save
+      Cart.destroy(session[:cart_id]) #or current_cart.id
+      session[:cart_id] = nil
+      redirect_to root_url, notice: 'Thank you for your order'
+    else
+      @cart = current_cart
+      render :new
+    end
+  end
+  
+  private
+    def order_params
+      params.require(:order).permit(:name, :phone, :email, :status)
+    end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -13,7 +13,13 @@
 
 class Order < ApplicationRecord
     has_many :order_products, dependent: :destroy
-    has_many :products, through: :order_products
     
     validates :name, :email, :phone, presence: true
+    
+    def add_order_product_from_cart(cart)
+      cart.order_products.each do |item|
+         item.cart_id = nil
+         order_products << item
+      end
+    end
 end
