8fef507fd2c687131204635f3bcaf7550bb8ce8f
Adamenko <mangamen45@gmail.com>
2017-01-31 22:01:45 +0200

    rspec

--- spec/controllers/order_products_controller_spec.rb
+++ spec/controllers/order_products_controller_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe OrderProductsController, type: :controller do
     it 'have increase products in cart' do
       count_of_products_in_cart = session[:count_of_products]
       # order_products = OrderProduct.create(product_id: 1, quantity: 1, cart_id: 2)
-      post "/add_same_to_cart"
+      post "add_same_to_cart"
       expect(count_of_products_in_cart).to eq(session[:count_of_products].to_i + 1)
+      # Started POST "/order_products/53/add_same_to_cart" for 127.0.0.1 at 2017-01-31 22:00:51 +0200
+      # Processing by OrderProductsController#add_same_to_cart as JS
+      # Parameters: {"id"=>"53"}
     end
   end
 end
